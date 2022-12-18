fec70fb4db4260cfc70158189f20f565af4a0dfd
Martin Broede <martinbro2021@yahoo.com>
2022-12-18 12:46:48 +0100

    clean up settings

--- cmb/cmb_sample/settings.py
+++ cmb/cmb_sample/settings.py
@@ -9,18 +9,15 @@ logger = logging.getLogger(__name__)
 # Build paths inside the cmb_sample like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 DEBUG = True
+
+ALLOWED_HOSTS = ["localhost"]
 
 SECRET_KEY = os.environ.get("SECRET_KEY_CMS")
 if not SECRET_KEY and not DEBUG:
     raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
 elif not SECRET_KEY:
     SECRET_KEY = get_random_secret_key()
-
-ALLOWED_HOSTS = []
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -70,18 +67,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cmb_sample.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -98,33 +89,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Berlin'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # deployment path
 LOCAL_APPS = [app for app in INSTALLED_APPS if app.count(".") == 0]  # only temp
 STATICFILES_DIRS = [os.path.join(BASE_DIR, f"{app}/static") for app in LOCAL_APPS]  # development paths
 STATICFILES_DIRS.append(os.path.join(BASE_DIR, "cmb_sample/static"))
 STATIC_URL = 'static/'  # url used in deployment and development
-
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # deployment path
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
