d673e565015f741685e54343dc3b36d1449387a3
Martin Broede <martinbro2021@yahoo.com>
2022-12-18 13:06:04 +0100

    clean up urls

--- cmb/cmb_sample/urls.py
+++ cmb/cmb_sample/urls.py
@@ -1,6 +1,6 @@
 # For more information please see https://docs.djangoproject.com/en/4.1/topics/http/urls/
 
-from django.conf import settings
+from .settings import DEBUG, MEDIA_ROOT, MEDIA_URL
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -11,6 +11,5 @@ urlpatterns = [
     path('contact/', include("cmb_contact.urls"))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
