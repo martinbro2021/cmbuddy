d221e0439c8cd42e7d01624551dd61ddf5250d89
Martin Broede <martinbro2021@yahoo.com>
2022-12-27 08:48:50 +0100

    remove print statements

--- cmb/cmb_contact/mail_wrapper.py
+++ cmb/cmb_contact/mail_wrapper.py
@@ -6,8 +6,6 @@ from cmb_home.models import Setting
 def send_mail_wrapper(post):
     # noinspection PyUnresolvedReferences
     recipients = [address.strip() for address in Setting.objects.get(key="contact_email_recipients").value.split(",")]
-    print("will send", post)
-    print("to", recipients)
     send_mail(
         post["subject"],
         post["message"],
