554fca7ec5e14cf0a473f9f6e21b19b44fe1db11
Martin Broede <martinbro2021@yahoo.com>
2022-12-29 23:23:55 +0100

    bugfix rebuild.ps1

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,14 +1,15 @@
 # shortcut for rebuilding the database
 # only for development/debugging purposes
 
-echo "Fresh random secret key"
-python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
-
 Remove-Item -Recurse .\cmb\cmb_home\migrations
 Remove-Item -Recurse .\cmb\cmb_contact\migrations
 Remove-Item .\cmb\sample-db.sqlite3
 
 .\.venv.dev\Scripts\Activate.ps1
+
+echo "Fresh random secret key"
+python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'
+
 python .\cmb\manage.py makemigrations cmb_home
 python .\cmb\manage.py makemigrations cmb_contact
 python .\cmb\manage.py migrate
